de0db672a4956db4cbf915d807e3aa9c797deac0
Gomaz A. <pogis@inbox.ru>
2026-02-03 10:49:47 +0300

    Fixed the issues

--- src/test/java/ru/netology/qa/SelenideTest.java
+++ src/test/java/ru/netology/qa/SelenideTest.java
@@ -9,38 +9,50 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class SelenideTest {
 
 
-    LocalDate date = LocalDate.now().plusDays(5);
+    LocalDate date = LocalDate.now().plusDays(7);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
     String dateText = date.format(formatter);
-
-    LocalDate date7 = LocalDate.now().plusDays(7);
-    String dayClick = String.valueOf(date7.getDayOfMonth());
-
+    String dayClick = String.valueOf(date.getDayOfMonth());
 
 
     @Test
-    void shouldSubmitForm() {
+    void ShouldSubmitForm() {
+        Selenide.open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(dateText);
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+71234512345");
+        $("[data-test-id='agreement'] span").click();
+        $(".button__content").click();
+        $("[data-test-id= 'notification'] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(visible)
+                .shouldHave(text("успешно забронирована"));
+    }
+
+    @Test
+    void InteractingActiveElements() {
         Selenide.open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Мо");
         $$(".menu-item").findBy(text("Москва")).click();
         $(".icon_name_calendar").click();
         $$(".calendar__day").findBy(text(dayClick)).click();
-//        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
-//        $("[data-test-id='date'] input").setValue(dateText);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+71234512345");
         $("[data-test-id='agreement'] span").click();
         $(".button__content").click();
-        $("[data-test-id= 'notification']")
+        $("[data-test-id= 'notification'] .notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("Успешно!"));
+                .shouldBe(visible)
+                .shouldHave(text("успешно забронирована"));
+
     }
 }
